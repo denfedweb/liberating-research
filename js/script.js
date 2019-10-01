7316af653bfc011cd583b25694385be204cbf95b
Denis Fedorovici <denfedweb@mail.ru>
2019-10-01 17:34:35 +0300

    com

--- js/script.js
+++ js/script.js
@@ -1,15 +1,38 @@
 $(document).ready(function() {
 // логика селектов (врзможно нужно переделать)
-    $('.js-dropdown').SumoSelect({search: true, searchText: 'Enter here.', placeholder: 'This is a placeholder'});
+    
+    $('.js-dropdown').select2({
+        placeholder: ''
+    });
+    
+// adaptive logic
     $('.menu-link').click(function(){
         $(this).toggleClass('menu-link_active');
         $('.adaptiveMenu').toggle();
     });
 
+//  логика попапа export 
+$('.btn-popup-export').click(function(){
+    $('.popup-export').css('display', 'flex');
+})
+$('.popup-export-close').click(function(){
+    $('.popup-export').toggle();
+})
 
+// логика попапа send message
+$('.btn-sendMessage').click(function(){
+    $('.popup-content').slideToggle();
+    
+})
+$('.popup-close').click(function(){
+    $('.popup-content').slideToggle();
+})
 
-
-
+// логика добавление селектов на кнопку
+   $('.collection-add-btn').click(function(){
+       console.log( $(this).prev('select'))
+      
+   })
 // логика раскрывания таблицы в адаптивном виде
     $(".tab-mobile").click(function() {
         $(this).next('div').slideToggle();
